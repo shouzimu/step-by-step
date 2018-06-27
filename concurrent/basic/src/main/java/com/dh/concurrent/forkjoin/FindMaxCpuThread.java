6a69f8ca4fb0c63f88bb94a7316e14a5e8f23a27
lihong <lihong@liangyibang.com>
2018-06-27 17:01:17 +0800

    add concurrent module

---
+++ concurrent/basic/src/main/java/com/dh/concurrent/forkjoin/FindMaxCpuThread.java
@@ -0,0 +1,32 @@
+package com.dh.concurrent.forkjoin;
+
+import java.util.stream.IntStream;
+
+public class FindMaxCpuThread {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (true) {
+                for (int i = 0; i < 10; i++) {
+                    int a = 1000 * 1000;
+                    a = a * 199;
+                }
+            }
+        }).start();
+
+        IntStream.rangeClosed(0,5).forEach((i)->{
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1000*600);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
+
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
